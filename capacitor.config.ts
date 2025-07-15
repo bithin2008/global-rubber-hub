401a071c509540dbb02c79bfbec51532489def77
BITHIN <bithin.softronet@gmail.com>
2025-07-16 01:25:09 +0530

    files added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'global-rubber-hubpp',
+  webDir: 'www'
+};
+
+export default config;
