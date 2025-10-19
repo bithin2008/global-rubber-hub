9ee80334320a54afcd41a42e7418ee64018f8f40
BITHIN <bithin2008@gmail.com>
2025-10-19 10:57:45 +0530

    update

--- src/app/shared/image-lightbox/image-lightbox.component.ts
+++ src/app/shared/image-lightbox/image-lightbox.component.ts
@@ -14,7 +14,7 @@ import { register } from 'swiper/element/bundle';
 export class ImageLightboxComponent implements OnInit {
   @Input() images: string[] = [];
   @Input() startIndex: number = 0;
-
+  public fallbackImg: string = 'https://globalrubberhub.com/public/backend/assets/images/default_item_image.jpeg';
   constructor(private modalController: ModalController) {}
 
   ngOnInit(): void {
@@ -23,6 +23,13 @@ export class ImageLightboxComponent implements OnInit {
 
   dismiss(): void {
     this.modalController.dismiss();
+  }
+
+  onThumbnailError(event: Event) {
+    const target = event.target as HTMLImageElement | null;
+    if (target && target.src !== this.fallbackImg) {
+      target.src = this.fallbackImg;
+    }
   }
 }
 
